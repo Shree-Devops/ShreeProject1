ea9810b0c96a3e0ec6fb51233dde3bc91d6e5274
ranubaghel671994@gmail.com <ranubaghel671994@gmail.com>
2019-01-10 22:46:05 +0530

    Shree is changed to satish

--- src/com/shreeAutomation/ShreeAutoMain.java
+++ src/com/shreeAutomation/ShreeAutoMain.java
@@ -3,7 +3,7 @@ package com.shreeAutomation;
 public class ShreeAutoMain {
 
 	public static void main(String[] args) {
-		System.out.println("Shree is saying haaaaaaaaaai");
+		System.out.println("Satish is saying haaaaaaaaaai");
 	}
 
 }
