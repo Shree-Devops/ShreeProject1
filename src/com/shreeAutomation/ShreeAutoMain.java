c616f53214b7bd0a6606b641b53986cc7f831162
ranubaghel671994@gmail.com <ranubaghel671994@gmail.com>
2019-01-10 22:58:04 +0530

    Mukul is replaced by pinky

--- src/com/shreeAutomation/ShreeAutoMain.java
+++ src/com/shreeAutomation/ShreeAutoMain.java
@@ -3,7 +3,7 @@ package com.shreeAutomation;
 public class ShreeAutoMain {
 
 	public static void main(String[] args) {
-		System.out.println("Mukul is saying haaaaaaaaaai");
+		System.out.println("Pinky is saying haaaaaaaaaai");
 	}
 
 }
